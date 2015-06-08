a2dcf51ebee41ce7ee90ee0cb758dc7c7eab429d
Joshua Beam <joshua.a.beam@gmail.com>
2015-06-07 21:09:10 -0500

    fixed dir

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express'),
 	app = express(),
 	port = process.env.PORT || 5000;
 
-app.use(express.static('app/dist'));
-app.use(express.static('app/templates'));
+app.use(express.static(__dirname + '/app/dist'));
+app.use(express.static(__dirname + '/app/templates'));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/app/index.html');
