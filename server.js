f66fb180edee345c68405d9f9b37f1dd3be7bebb
Joshua Beam <joshua.a.beam@gmail.com>
2015-06-07 21:14:01 -0500

    process cwd instead of dirname

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express'),
 	app = express(),
 	port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/app/dist'));
-app.use(express.static(__dirname + '/app/templates'));
+process.env.PWD = process.cwd();
+
+app.use(express.static(process.env.PWD + '/app/dist'));
+app.use(express.static(process.env.PWD + '/app/templates'));
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/app/index.html');
+	res.sendFile(process.env.PWD + '/app/index.html');
 });
 
 app.listen(port, function() {
